d5031a433a57019f57f270d4156737dff57502e3
Md Repon <repon7253@gmail.com>
2025-07-29 14:54:34 +0600

    project setup complete

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
-import express from "express"
-
+import express, { Request, Response } from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import notFound from "./app/middlewares/notFound"
 
 const app = express()
 
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Welcome to Ride Booking system backend!!!"
+    })
+})
+
+app.use(notFound)
 
 export default app
---
+++ src/app/config/env.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+interface EnvConfig {
+    PORT: string;
+    DB_URL: string;
+    NODE_ENV: string;
+}
+
+const loadEnvVariables = (): EnvConfig => {
+    const requiredEnvVariables: string[] = [
+        "PORT",
+        "DB_URL",
+        "NODE_ENV"
+    ]
+
+    requiredEnvVariables.forEach(key => {
+        if (!process.env[key]) {
+            throw new Error(`Missing required environment variable ${key}`)
+        }
+    })
+
+    return {
+        PORT: process.env.PORT!,
+        DB_URL: process.env.DB_URL!,
+        NODE_ENV: process.env.NODE_ENV!
+    }
+}
+
+export const envVars: EnvConfig = loadEnvVariables()
