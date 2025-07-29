6381143df8b6d6b8b5ca147b0652335666ae70c4
Md Repon <repon7253@gmail.com>
2025-07-29 14:19:22 +0600

    fix lint issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,21 @@
-// @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  //   tseslint.configs.recommended
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     rules: {
-      // Unused imports
       'unused-imports/no-unused-imports': 'warn',
       'unused-imports/no-unused-vars': [
         'warn',
         { vars: 'all', args: 'after-used', argsIgnorePattern: '^_' },
       ],
-
       'no-console': 'warn',
     },
   }
