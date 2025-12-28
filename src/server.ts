6c7b71c3dab2f0ee9e0eb4763124454f33ea9b7f
Md Repon <repon7253@gmail.com>
2025-12-28 14:13:18 +0600

    fix deployment issue

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import app from './app';
 import { envVars } from './app/config/env';
 import { seedSuperAdmin } from './app/utils/seedSuperAdmin';
-// import { connectRedis } from './app/config/radis.config';
+import { connectRedis } from './app/config/radis.config';
 
 
 let server: Server;
@@ -25,7 +25,7 @@ const startServer = async () => {
 
 (
     async () => {
-        // await connectRedis()
+        await connectRedis()
         await startServer()
         await seedSuperAdmin()
     }
