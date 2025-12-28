df3a72875f66420f49ba1259c3d87fc0f40bf711
Md Repon <repon7253@gmail.com>
2025-12-28 15:40:18 +0600

    add maxAge

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: envVars.FRONTEND_URL,
+    origin: [envVars.FRONTEND_URL, "http://localhost:3000"],
     credentials: true
 }))
 
--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -12,7 +12,8 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
         res.cookie("accessToken", tokenInfo.accessToken, {
             httpOnly: true,
             secure: true,
-            sameSite: "none"
+            sameSite: "none",
+            maxAge: 24 * 60 * 60 * 1000,
         })
     }
 
@@ -20,7 +21,8 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
         res.cookie("refreshToken", tokenInfo.refreshToken, {
             httpOnly: true,
             secure: true,
-            sameSite: "none"
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         })
     }
 }
